525c6f84b10c19805df517868232ecbd7c0ad983
ABDUL SIRAJUDEEN S <110275087+abdsirajoffical@users.noreply.github.com>
2024-07-13 20:58:33 +0530

    aws1

--- src/index.ts
+++ src/index.ts
@@ -16,17 +16,15 @@ app.use(express.urlencoded({ extended: true }))
 
 const PORT = process.env.PORT || 3001;
 
-
-
-app.use('/', async (req, res) => {
-    console.log("hello world");
-    return res.json({ message: "Hello World" });
-});
-
 //routers
 app.use('/product', productRouter);
 app.use('/user', loginRouter);
 app.use('/billing', billingRouter);
 app.use('/reports', reportRouter);
+
+app.use('/sample', async (req, res) => {
+    console.log("hello world");
+    return res.json({ message: "Hello World" });
+});
 
 app.listen(PORT)
